949dca05af6c8d4b9d12943d2931f8b3d5130b54
DimitarSlezenkovski <dslezenkovski@gmail.com>
2021-04-18 01:11:37 +0200

    update

--- app.py
+++ app.py
@@ -8,15 +8,17 @@ import datetime
 from datetime import datetime
 import math
 
+
 def saveUserLocation(locationBody):
     new_location = Location(
-        lng = locationBody['lng'],
-        lat = locationBody['lat'],
-        userId = locationBody['userId']
+        lng=locationBody['lng'],
+        lat=locationBody['lat'],
+        userId=locationBody['userId']
     )
     db.session.add(new_location)
     db.session.commit()
     return True
+
 
 # get from user ms
 # def get_all_users_key_value_from_user_ms(users_listBody):
@@ -29,6 +31,7 @@ def get_all_user_friends(user_id):
         friends_list.append({"user1Id": friend.user1Id, "user2Id": friend.user2Id})
     return {'friends': friends_list}
 
+
 def get_user_timeline(user_id):
     # user_feed = Post.query.filter_by(userId=user_id).all()
     user_feed = db.session.query(Post).filter_by(userId=user_id).all()
@@ -36,6 +39,7 @@ def get_user_timeline(user_id):
     for post in user_feed:
         user_post_feed.append({'userId': post.userId, 'text': post.text, 'comments': post.comments})
     return {'timeline': user_post_feed}
+
 
 def get_global_feed(user_id):
     posts = []
@@ -51,7 +55,8 @@ def get_global_feed(user_id):
         for post in posts:
             user_posts.append({'userId': post.userId, 'text': post.text, 'comments': post.comments})
 
-    return {'posts': user_posts }
+    return {'posts': user_posts}
+
 
 def get_all_user_friend_requests(user_id):
     reqs = db.session.query(FriendRequest).filter(toUserId=user_id)
@@ -64,37 +69,45 @@ def get_all_user_friend_requests(user_id):
 def getAllUsersLocation():
     locations = db.session.query(Location).filter_by(isCycleService=False).all()
     json_locations = []
-    for i in locations
-        json_locations.append({'id': i.id, 'lat': i.lat, 'lng': i.lng, 'userId': i.userId, 'isCycleService': i.isCycleService})
+    for i in locations:
+        json_locations.append(
+            {'id': i.id, 'lat': i.lat, 'lng': i.lng, 'userId': i.userId, 'isCycleService': i.isCycleService})
     return {'locations': json_locations}
+
 
 def getEcycleServices():
     locations = db.session.query(Location).filter_by(isCycleService=True).all()
     json_locations = []
-    for i in locations
-        json_locations.append({'id': i.id, 'lat': i.lat, 'lng': i.lng, 'userId': i.userId, 'isCycleService': i.isCycleService})
+    for i in locations:
+        json_locations.append(
+            {'id': i.id, 'lat': i.lat, 'lng': i.lng, 'userId': i.userId, 'isCycleService': i.isCycleService})
     return {'locations': json_locations}
 
-def getCycleHistory(userId)
+
+def getCycleHistory(userId):
     routes = db.session.query(CycledRoute).all()
     json_routes = []
-    for i in routes
-        json_routes.append({'id': i.id, 'userId': i.userId, 'distanceTraveled': i.distanceTraveled, 'caloriesBurned': i.caloriesBurned, 'route': i.route})
+    for i in routes:
+        json_routes.append(
+            {'id': i.id, 'userId': i.userId, 'distanceTraveled': i.distanceTraveled, 'caloriesBurned': i.caloriesBurned,
+             'route': i.route})
     return {'routes': json_routes}
+
 
 def editPost(postBody):
     post = db.session.query(Post).get(postBody['id'])
     if post:
-        if postBody['text']
+        if postBody['text']:
             post.text = postBody['text']
-        if postBody['image']
+        if postBody['image']:
             post.image = postBody['image']
         db.session.commit()
         return True
     else:
         return {'error': 'Post not found'}, 404
 
-def deletePost(postId)
+
+def deletePost(postId):
     post = db.session.query(Post).filter_by(id=postId).one()
     db.session.query(Post).delete(post)
     db.session.commit()
@@ -104,153 +117,165 @@ def deletePost(postId)
     else:
         return True
 
+
 def deleteComment(delCommentBody):
     return
 
+
 def deleteCyclingParty(delPartyBody):
-    creatorId = CycleParty.query.filter_by(id = delPartyBody['partyId']).first().partyCreatorId
-    if delPartyBody['userId'] == creatorId :
-        partyToDelete = CycleParty.query.filter_by(id = delPartyBody['partyId']).first()
+    creatorId = CycleParty.query.filter_by(id=delPartyBody['partyId']).first().partyCreatorId
+    if delPartyBody['userId'] == creatorId:
+        partyToDelete = CycleParty.query.filter_by(id=delPartyBody['partyId']).first()
         CyclePartyMember.query.filter_by(partyId=delPartyBody['partyId']).delete()
         db.session.delete(partyToDelete)
         db.session.commit()
+
 
 def addCycledRoute(cycledRouteBody):
     # calculating the distance traveled
     # approximation of the radius of the earth in km
     R = 6373.0
 
-    latFrom = math.radians(float(Route.query.filter_by(id = cycledRouteBody['route']).first().latFrom))
-    lngFrom = math.radians(float(Route.query.filter_by(id = cycledRouteBody['route']).first().lngFrom))
-    latTo = math.radians(float(Route.query.filter_by(id = cycledRouteBody['route']).first().latTo))
-    lngTo = math.radians(float(Route.query.filter_by(id = cycledRouteBody['route']).first().lngTo))
-    
+    latFrom = math.radians(float(Route.query.filter_by(id=cycledRouteBody['route']).first().latFrom))
+    lngFrom = math.radians(float(Route.query.filter_by(id=cycledRouteBody['route']).first().lngFrom))
+    latTo = math.radians(float(Route.query.filter_by(id=cycledRouteBody['route']).first().latTo))
+    lngTo = math.radians(float(Route.query.filter_by(id=cycledRouteBody['route']).first().lngTo))
+
     deltaLng = lngTo - lngFrom
     deltaLat = latFrom - latTo
 
     # using the haversine formula to determine the distance which the user traveled
-    a = (math.sin(deltaLat/2))**2 + math.cos(latFrom) * math.cos(latTo) * (math.sin(deltaLng/2))**2
-    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
+    a = (math.sin(deltaLat / 2)) ** 2 + math.cos(latFrom) * math.cos(latTo) * (math.sin(deltaLng / 2)) ** 2
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
 
     distance = R * c
 
-    # burned calories = ((MET * userWeight * 3.5) / 200) * cycledTime 
-    # where the MET values are provided by "The Compendium of Physical Activities 2011" 
+    # burned calories = ((MET * userWeight * 3.5) / 200) * cycledTime
+    # where the MET values are provided by "The Compendium of Physical Activities 2011"
     MET = 1
-    
+
     if distance <= 8.8:
         MET = 4
-    elif distance >= 9 and distance <= 16 : 
+    elif distance >= 9 and distance <= 16:
         MET = 6.3
-    elif distance >= 17 and distance < 24 :
+    elif distance >= 17 and distance < 24:
         MET = 7.4
-    elif distance >= 24 and distance < 30 :
+    elif distance >= 24 and distance < 30:
         MET = 10
-    else : 
+    else:
         MET = 20
 
     burnedCalories = ((MET * float(cycledRouteBody['userWeight']) * 3.5) / 200) * float(cycledRouteBody['cycledTime'])
 
     new_cycledRoute = CycledRoute(
-        userId = cycledRouteBody['userId'],
-        distanceTraveled = str(distance),
-        userWeight = cycledRouteBody['userWeight'],
-        cycledTime = float(cycledRouteBody['cycledTime']),
-        caloriesBurned = burnedCalories,
-        route = cycledRouteBody['route']
+        userId=cycledRouteBody['userId'],
+        distanceTraveled=str(distance),
+        userWeight=cycledRouteBody['userWeight'],
+        cycledTime=float(cycledRouteBody['cycledTime']),
+        caloriesBurned=burnedCalories,
+        route=cycledRouteBody['route']
     )
 
     db.session.add(new_cycledRoute)
     db.session.commit()
 
+
 def leaveParty(leavePartyBody):
-    #TODO : If the party creator decides to leave the party then delete the entire party (I'll do it tmrw)
+    # TODO : If the party creator decides to leave the party then delete the entire party (I'll do it tmrw)
     partyToLeave = CyclePartyMember.query.filter_by(
-        userId = leavePartyBody['userId'],
-        partyId = leavePartyBody['partyId']
-        ).first()
+        userId=leavePartyBody['userId'],
+        partyId=leavePartyBody['partyId']
+    ).first()
     db.session.delete(partyToLeave)
-    
-    #check if the party is now empty, if it is delete it from the db
-    party = CyclePartyMember.query.filter_by(partyId = leavePartyBody['partyId']).first()
+
+    # check if the party is now empty, if it is delete it from the db
+    party = CyclePartyMember.query.filter_by(partyId=leavePartyBody['partyId']).first()
     if party is None:
-        partyToDelete = CycleParty.query.filter_by(id = leavePartyBody['partyId']).first()
+        partyToDelete = CycleParty.query.filter_by(id=leavePartyBody['partyId']).first()
         db.session.delete(partyToDelete)
 
     db.session.commit()
 
+
 def createPost(postBody):
     new_post = Post(
-        userId = postBody['userId'], 
-        text = postBody['text'], 
-        createdOn = datetime.now())
+        userId=postBody['userId'],
+        text=postBody['text'],
+        createdOn=datetime.now())
     db.session.add(new_post)
     db.session.commit()
 
+
 def postComment(commentBody):
     new_comment = Comment(
-        postId = commentBody['postId'],
-        userId = commentBody['userId'],
-        text = commentBody['text'],
-        createdOn = datetime.now()
+        postId=commentBody['postId'],
+        userId=commentBody['userId'],
+        text=commentBody['text'],
+        createdOn=datetime.now()
     )
     db.session.add(new_comment)
     db.session.commit()
 
+
 def addRoute(routeBody):
     new_route = Route(
-        lngFrom = routeBody['lngFrom'],
-        latFrom = routeBody['latFrom'],
-        lngTo = routeBody['lngTo'],
-        latTo = routeBody['latTo']
-        )
+        lngFrom=routeBody['lngFrom'],
+        latFrom=routeBody['latFrom'],
+        lngTo=routeBody['lngTo'],
+        latTo=routeBody['latTo']
+    )
     db.session.add(new_route)
     db.session.commit()
 
+
 def createCycleParty(cyclePartyBody):
     new_cycleParty = CycleParty(
-        route = cyclePartyBody['routeId'], 
-        partyCreatorId = cyclePartyBody['partyCreatorId'])
+        route=cyclePartyBody['routeId'],
+        partyCreatorId=cyclePartyBody['partyCreatorId'])
     db.session.add(new_cycleParty)
 
     db.session.commit()
     first_member = CyclePartyMember(
-        partyId = CycleParty.query.filter_by(partyCreatorId=cyclePartyBody['partyCreatorId']).first().id,
-        userId = cyclePartyBody['partyCreatorId'],
+        partyId=CycleParty.query.filter_by(partyCreatorId=cyclePartyBody['partyCreatorId']).first().id,
+        userId=cyclePartyBody['partyCreatorId'],
     )
     db.session.add(first_member)
 
     db.session.commit()
 
+
 def sendMessage(msgBody):
-    if friendRequestBody['fromUserId'] != friendRequestBody['toUserId'] :
+    if msgBody['fromUserId'] != msgBody['toUserId']:
         d = datetime.now()
         new_message = Message(
-            fromUserId=msgBody['fromUserId'], 
-            toUserId=msgBody['toUserId'], 
-            dateSent=d, 
+            fromUserId=msgBody['fromUserId'],
+            toUserId=msgBody['toUserId'],
+            dateSent=d,
             text=msgBody['text'])
         db.session.add(new_message)
         db.session.commit()
 
+
 def sendFriendRequest(friendRequestBody):
-    if friendRequestBody['fromUserId'] != friendRequestBody['toUserId'] :
+    if friendRequestBody['fromUserId'] != friendRequestBody['toUserId']:
         new_friendRequest = FriendRequest(
-            fromUserId = friendRequestBody['fromUserId'],
-            toUserId = friendRequestBody['toUserId'])
+            fromUserId=friendRequestBody['fromUserId'],
+            toUserId=friendRequestBody['toUserId'])
         db.session.add(new_friendRequest)
         db.session.commit()
 
+
 def ackFriendRequest(friendBody):
-    if friendBody['requestSender'] != friendBody['requestRecepient'] :
+    if friendBody['requestSender'] != friendBody['requestRecepient']:
         if friendBody['resp'] == True:
-            new_friends = Friends(user1Id = friendBody['requestSender'], user2Id = friendBody['requestRecepient'])
+            new_friends = Friends(user1Id=friendBody['requestSender'], user2Id=friendBody['requestRecepient'])
             db.session.add(new_friends)
-            FriendRequest.query.filter_by(fromUserId = friendBody['requestSender']).delete()
+            FriendRequest.query.filter_by(fromUserId=friendBody['requestSender']).delete()
             db.session.commit()
-        else :
-            FriendRequest.query.filter_by(fromUserId = friendBody['requestSender']).delete()
+        else:
+            FriendRequest.query.filter_by(fromUserId=friendBody['requestSender']).delete()
             db.session.commit()
+
 
 connexion_app = connexion.App(__name__, specification_dir="./")
 app = connexion_app.app
--- models.py
+++ models.py
@@ -5,23 +5,27 @@ from sqlalchemy import DateTime
 from friendshipStatus import Status
 import uuid
 
+
 class Friends(db.Model):
     __tablename__ = 'friends'
-    user1Id = db.Column(db.BigInteger, nullable = False, primary_key = True)
-    user2Id = db.Column(db.BigInteger, nullable = False, primary_key = True)
+    user1Id = db.Column(db.BigInteger, nullable=False, primary_key=True)
+    user2Id = db.Column(db.BigInteger, nullable=False, primary_key=True)
+
 
 class FriendRequest(db.Model):
     __table__name = 'friend_requests'
-    fromUserId = db.Column(db.BigInteger, nullable = False, primary_key = True)
-    toUserId = db.Column(db.BigInteger, nullable = False, primary_key = True)
-    status = db.Column(db.Enum(Status), default = Status.PENDING)
+    fromUserId = db.Column(db.BigInteger, nullable=False, primary_key=True)
+    toUserId = db.Column(db.BigInteger, nullable=False, primary_key=True)
+    status = db.Column(db.Enum(Status), default=Status.PENDING)
+
 
 class Message(db.Model):
     __tablename__ = 'messages'
-    fromUserId = db.Column(db.BigInteger, nullable = False, primary_key = True)
-    toUserId = db.Column(db.BigInteger, nullable = False, primary_key = True)
-    dateSent = db.Column(db.DateTime, nullable = False, primary_key = True)
-    text = db.Column(db.String, nullable = False)
+    fromUserId = db.Column(db.BigInteger, nullable=False, primary_key=True)
+    toUserId = db.Column(db.BigInteger, nullable=False, primary_key=True)
+    dateSent = db.Column(db.DateTime, nullable=False, primary_key=True)
+    text = db.Column(db.String, nullable=False)
+
 
 class Post(db.Model):
     __tablename__ = 'posts'
@@ -43,20 +47,22 @@ class Comment(db.Model):
     text = db.Column(db.String, nullable=False)
     createdOn = db.Column(db.DateTime, nullable=False)
     generatedId = str(uuid.uuid4().hex)
-    id = db.Column(db.String, nullable = False ,default=generatedId, primary_key = True)
-    userId = db.Column(db.BigInteger, nullable = False)
-    text = db.Column(db.String, nullable = False)
-    comments = db.relationship("Comment", backref = 'post')
-    createdOn = db.Column(db.DateTime, nullable = False)
-    
+    id = db.Column(db.String, nullable=False, default=generatedId, primary_key=True)
+    userId = db.Column(db.BigInteger, nullable=False)
+    text = db.Column(db.String, nullable=False)
+    comments = db.relationship("Comment", backref='post')
+    createdOn = db.Column(db.DateTime, nullable=False)
+
+
 class Comment(db.Model):
     __tablename__ = 'comments'
     generatedId = str(uuid.uuid4().hex)
-    id = db.Column(db.String, nullable = False ,default=generatedId, primary_key = True)
+    id = db.Column(db.String, nullable=False, default=generatedId, primary_key=True)
     postId = db.Column(db.String, db.ForeignKey('posts.id'))
-    userId = db.Column(db.BigInteger, nullable = False)
-    text = db.Column(db.String, nullable = False)
-    createdOn = db.Column(db.DateTime, nullable = False)
+    userId = db.Column(db.BigInteger, nullable=False)
+    text = db.Column(db.String, nullable=False)
+    createdOn = db.Column(db.DateTime, nullable=False)
+
 
 class Route(db.Model):
     __tablename__ = 'route'
@@ -67,6 +73,7 @@ class Route(db.Model):
     lngTo = db.Column(db.String, nullable=False)
     latTo = db.Column(db.String, nullable=False)
 
+
 class Location(db.Model):
     __tablename__ = 'location'
     generatedId = uuid.uuid1()
@@ -75,6 +82,7 @@ class Location(db.Model):
     lat = db.Column(db.String, nullable=False)
     userId = db.Column(db.BigInteger, nullable=False)
     isCycleService = db.Column(db.Boolean, nullable=True)
+
 
 class CycledRoute(db.Model):
     __tablename__ = 'cycled_routes'
@@ -91,6 +99,7 @@ class CycledRoute(db.Model):
     caloriesBurned = db.Column(db.BigInteger)
     route = db.Column(db.String, db.ForeignKey('route.id'), nullable=False)
 
+
 class CycleParty(db.Model):
     __tablename__ = 'cycle_party'
     generatedId = str(uuid.uuid4().hex)
@@ -104,12 +113,13 @@ class CyclePartyMember(db.Model):
     __tablename__ = 'cycle_party_member'
     partyId = db.Column(db.String, db.ForeignKey('cycle_party.id'), nullable=False, primary_key=True)
     userId = db.Column(db.BigInteger, nullable=False, primary_key=True)
-    id = db.Column(db.String, nullable = False ,default=generatedId, primary_key = True)
-    route = db.Column(db.String, db.ForeignKey('route.id'), nullable = False)
-    partyCreatorId = db.Column(db.BigInteger, nullable = False)
-    members = db.relationship("CyclePartyMember", backref = 'post')
+    id = db.Column(db.String, nullable=False, default=generatedId, primary_key=True)
+    route = db.Column(db.String, db.ForeignKey('route.id'), nullable=False)
+    partyCreatorId = db.Column(db.BigInteger, nullable=False)
+    members = db.relationship("CyclePartyMember", backref='post')
+
 
 class CyclePartyMember(db.Model):
     __tablename__ = 'cycle_party_member'
-    partyId = db.Column(db.String, db.ForeignKey('cycle_party.id'), nullable = False, primary_key = True)
-    userId = db.Column(db.BigInteger, nullable = False, primary_key = True)
+    partyId = db.Column(db.String, db.ForeignKey('cycle_party.id'), nullable=False, primary_key=True)
+    userId = db.Column(db.BigInteger, nullable=False, primary_key=True)
